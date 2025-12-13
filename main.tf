34969322e309ed80e59c38d9529f62155425787e
JohnG37 <156236996+JohnG37@users.noreply.github.com>
2025-12-13 13:05:19 -0500

    Add Terraform configuration for AWS resources
    This Terraform configuration sets up an AWS provider, a security group allowing SSH and HTTP traffic, and an EC2 instance with specified parameters.

---
+++ main.tf
@@ -0,0 +1,73 @@
+##############################
+# Terraform + AWS Provider
+##############################
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+##############################
+# Security Group (allow HTTP)
+##############################
+resource "aws_security_group" "demo_sg" {
+  name_prefix = "demo-sg-"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  # SSH (required for Ansible)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  # HTTP for Web Server
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Terraform-Demo-SG"
+  }
+}
+
+##############################
+# EC2 Instance
+##############################
+resource "aws_instance" "demo_ec2" {
+  ami                    = "ami-08c40ec9ead489470" # Ubuntu 22.04 LTS (us-east-1)
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+
+  key_name = "terraform"
+
+  monitoring = true
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"  # Forces IMDSv2
+    http_put_response_hop_limit = 1
+  }
+
+  tags = {
+    Name = "Terraform-Demo-EC2"
+  }
+}
