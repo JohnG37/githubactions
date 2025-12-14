70aecb825248ecba2712ecfaa99ef68acb991ca0
JohnG37 <156236996+JohnG37@users.noreply.github.com>
2025-12-14 02:21:15 -0500

    Add output for EC2 instance public IP
    Add output for the public IP address of the EC2 instance

--- main.tf
+++ main.tf
@@ -71,3 +71,11 @@ resource "aws_instance" "demo_ec2" {
     Name = "Terraform-Demo-EC2"
   }
 }
+
+##############################
+# Outputs
+##############################
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.demo_ec2.public_ip
+}
